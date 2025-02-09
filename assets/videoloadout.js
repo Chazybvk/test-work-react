2a3b947fe5015963feaab7e0f57f12b14f7cca2a
Konstantin Boltyshev <66037242+Chazybvk@users.noreply.github.com>
2025-02-10 00:28:47 +0300

    Update videoloadout.js

--- assets/videoloadout.js
+++ assets/videoloadout.js
@@ -2,6 +2,7 @@ let lastWidth = window.innerWidth; // Сохраняем последнюю ши
 
 function checkScreenWidth() {
   var video = document.getElementsByTagName("video")[0];
+  var source = document.getElementsByTagName("source")[0]; // Получаем первый элемент source
   var currentWidth = window.innerWidth;
 
   // Добавляем атрибут playsinline только при первой загрузке страницы
@@ -12,6 +13,17 @@ function checkScreenWidth() {
   // Выполняем действия только при переходе через 530px
   if ((lastWidth <= 530 && currentWidth > 530) || (lastWidth > 530 && currentWidth <= 530)) {
     video.pause();
+    video.load();
+    video.play();
+
+    // Меняем атрибут src у элемента source в зависимости от ширины экрана
+    if (currentWidth > 530) {
+      source.setAttribute('src', './stock-video.mp4');
+    } else {
+      source.setAttribute('src', './stock-video-mobile.mp4');
+    }
+
+    // Перезагружаем видео с новым источником
     video.load();
     video.play();
   }
