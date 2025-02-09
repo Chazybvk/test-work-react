638e79806107999f47704975ca83b121f4793fd3
Konstantin Boltyshev <66037242+Chazybvk@users.noreply.github.com>
2025-02-10 01:34:04 +0300

    Update videoloadout.js

--- assets/videoloadout.js
+++ assets/videoloadout.js
@@ -6,29 +6,23 @@ window.addEventListener("load", function() {
     // Устанавливаем атрибут playsinline для корректного отображения на iPhone
     video.setAttribute("playsinline", "true");
 
-    // Функция для изменения src в зависимости от ширины окна
     function updateSource() {
         if (window.innerWidth <= 530) {
             source.setAttribute("src", "./stock-video-mobile.mp4");
         } else {
             source.setAttribute("src", "./stock-video.mp4");
         }
-        // Перезагружаем видео, чтобы оно подхватило новый источник
         video.load();
     }
 
-    // Флаг для отслеживания времени между событиями resize
     let resizeTimeout;
 
-    // Функция для обработки изменения размера окна с задержкой
     function handleResize() {
-        // Если resize уже был запланирован, отменяем его
         if (resizeTimeout) {
             clearTimeout(resizeTimeout);
         }
         
-        // Планируем выполнение функции updateSource через 1 секунду (1000 миллисекунд)
-        resizeTimeout = setTimeout(updateSource, 1000);
+        resizeTimeout = setTimeout(updateSource, 500);
     }
 
     // Изменяем src при загрузке страницы
